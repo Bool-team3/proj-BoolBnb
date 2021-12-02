475cc7ab67358f23263952776ad88b8c415be918
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-12-02 18:48:18 +0100

    many-to-many relationship added

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -13,4 +13,8 @@ class Apartment extends Model
     public function user(){
         return $this->belongsTo('App\User');
     }
+
+    public function services(){
+        return $this->belongsToMany('App\Models\Service');
+    }
 }
