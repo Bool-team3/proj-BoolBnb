5d0a8dddcf69749033f2d5ec4fc2d082bd06be44
FrancescoSica16 <francesco.sica16@gmail.com>
2021-12-03 17:15:06 +0100

    add email fillable

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model
 {
-    protected $fillable = ['name','email', 'subject', 'message'];
+    protected $fillable = ['name','email_address', 'subject', 'message'];
 
     public function apartment(){
         return $this->belongsTo("App\Models\Apartment");
