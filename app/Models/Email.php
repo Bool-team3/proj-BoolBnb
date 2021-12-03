8cda70dee7e7d05f7785d4555a92c593b384e2c8
FrancescoSica16 <francesco.sica16@gmail.com>
2021-12-03 17:14:20 +0100

    add fillabe email

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Email extends Model
 {
+    protected $fillable = ['name','email', 'subject', 'message'];
+
     public function apartment(){
         return $this->belongsTo("App\Models\Apartment");
     }
