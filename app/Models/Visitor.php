f712d603215868f8af5b3c09b824688690e20633
FrancescoSica16 <francesco.sica16@gmail.com>
2021-12-03 17:59:29 +0100

    Add data to Visitors table

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visitor extends Model
 {
+    protected $fillable = ['date','ip'];
+    
     public function apartment(){
         return $this->belongsTo("App\Models\Apartment");
     }
--- database/seeds/VisitorSeeder.php
+++ database/seeds/VisitorSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
+use Illuminate\Support\Carbon;
+use App\Models\Apartment;
+use App\Models\Visitor;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
+use Faker\Generator as Faker;
 
 class VisitorSeeder extends Seeder
 {
@@ -9,8 +15,20 @@ class VisitorSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+       
+        $apartment_ids  = Apartment::pluck('id')->toArray();
+        
+        for($i=0 ; $i < count($apartment_ids) ; $i++){
+            
+            $newVisitor = new Visitor();
+            $newVisitor->apartment_id = Arr::random($apartment_ids);
+
+            $newVisitor->date = Carbon::now();
+            $newVisitor->ip = $faker->ipv4();
+
+            $newVisitor->save();
+        }
     }
 }
