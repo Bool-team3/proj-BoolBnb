5ead2ac6abeee92160efbbcb88d6f695434d4dfa
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-12-06 11:16:27 +0100

    collegamento routes

---
+++ resources/views/user/apartments/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>index</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi odio, ad, voluptas mollitia libero odit, saepe quibusdam repellat delectus nisi quos. Reiciendis facere voluptatibus quam voluptatem sapiente placeat ratione et?</p>
+
+@endsection
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -19,5 +19,6 @@
             </div>
         </div>
     </div>
+    <a href="{{route('user.apartments.index')}}">asda</a>
 </div>
 @endsection
