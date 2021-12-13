2eb91e48b6572458961df36ddaf223485896d6b4
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-12-13 11:35:49 +0100

    sium

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,10 @@
 @section('content')
 
         <div class="flex-center position-ref full-height">
-            {{-- @if(Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ route('user.home') }}">Dashboard</a>
-                    @endauth
-                </div>
-            @endif --}}
+            
             @if (Route::has('login'))
                     @auth
                         <a href="{{ route('user.home') }}">Home</a>
-                    {{-- @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif --}}
                     @endauth
                 </div>
             @endif
