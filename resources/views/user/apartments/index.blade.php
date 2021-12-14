5ddfd7bec5beb9e7cf9e143f20b36595533303e1
daviddpi <piscopodavid97@gmail.com>
2021-12-14 18:35:56 +0100

    add fontawesome + css by index apartments

--- resources/views/user/apartments/index.blade.php
+++ resources/views/user/apartments/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
     
-    <div class="container">
+    <div class="container" id="index-apartment">
         {{-- AVVISO IN CASO DI ELIMINAZIONE AVVENUTA CON SUCCESSO  --}}
         @if(session('delete'))
             <div class="alert alert-success" role="alert">
@@ -12,52 +12,78 @@
         @endif
 
         <div class="row">
-            <div class="col-12">
+            <div class="col-12 p-3">
                 <h2>I tuoi appartamenti</h2>
                 <a href="{{route('user.apartments.create')}}">Carica un nuovo appartamento</a>
                 @forelse ($apartments as $apartment)
-                    <div class="card-deck">
-                        <div class="card">
-                            <div class="d-flex">
-                                <img class="card-img-top img-fluid" style="width:300px" src="{{ $apartment->img_url ? $apartment->getImagePrefix() . $apartment->img_url : 'https://www.pianetacasafacile.it/uploads/cache/profile_mid/uploads/property_images/2018/01/property_no_photo.png' }}" alt="Appartamento">
+                    <div class="card w-75 mb-2">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <h5 class="card-title"><strong>Appartamento: </strong>{{$apartment->title}}</h5>
 
-                                <div class="card-body">
-                                    <h5 class="card-title"><a href="{{route("user.apartments.show", $apartment->id)}}">{{$apartment->title}}</a></h5>
-                                    <h6 class="card-title">Stanze: {{$apartment->room}}</h6>
-                                    <h6 class="card-title">Stanze da letto: {{$apartment->bedroom}}</h6>
-                                    <h6 class="card-title">Posti letto: {{$apartment->bed}}</h6>
-                                    <h6 class="card-title">Bagni: {{$apartment->bathroom}}</h6>
-                                    <h6 class="card-title">Metri quadrati: {{$apartment->mq}}</h6>
-                                    <p class="card-text">Città: {{$apartment->city}}</p>
-                                    <p class="card-text"><small class="text-muted">Last updated {{$apartment->created_at}} ago</small></p>
-                                    <a href="{{route('user.apartments.edit', $apartment)}}">Modifica Informazioni</a>
-                                    {{-- FORM PER ELIMINARE UN APPARTAMENTO  --}}
-                                    <form method="POST" action="{{route('user.apartments.destroy', $apartment)}}" class="delete-form" data-apartment-id="{{$apartment->id}}" data-apartment-title="{{$apartment->title}}">
-                                        @csrf
-                                        @method('DELETE')
-                                        {{-- BOTTONE CHE RICHIAMA UN 'POPUP' PER CONFERMARE DI VOLER ELIMINARE L'APPARTAMENTO  --}}
-                                        <input type="submit" value="Delete" onclick="return confirm('Sei sicura/o di voler eliminare questo appartamento?');">
-                                    </form> 
+                                    <p class="card-text mb-2">
+                                        Visibilità:
+                                        @if ($apartment->visible)
+                                            <i class="fas fa-eye" title="Visibile"></i>
+                                        @else
+                                            <i class="fas fa-eye-slash" title="Non visibile"></i>
+                                        @endif
+                                    </p>
+
+                                    <p class="card-text">
+                                        @if ($apartment->sponsors()->exists('sponsor_id'))
+                                            <span class="badge badge-success p-1">Sponsorizzato</span>
+                                        @else
+                                            <span class="badge badge-danger p-1">Non Sponsorizzato</span>
+                                        @endif
+                                    </p>
+                        
+                                    <p class="card-text"><small class="text-muted">Ultima modifica: {{$apartment->created_at}}</small></p>
+                                    <a type="button" class="btn btn-outline-info" href="{{route("user.apartments.show", $apartment->id)}}">Apri appartamento</a>
                                 </div>
+
+                                <div class="edit-delete-sponsor"> 
+                                    <div class="d-flex">
+                                        <div class="mr-3">
+                                            <a href="{{route('user.apartments.edit', $apartment)}}">
+                                                <i class="fas fa-edit btn btn-primary"  title="Modifica"></i>
+                                            </a>
+                                        </div>     
+                                        <div>
+                                            <form method="POST" action="{{route('user.apartments.destroy', $apartment)}}" class="delete-form" data-apartment-id="{{$apartment->id}}" data-apartment-title="{{$apartment->title}}">
+                                                @csrf
+                                                @method('DELETE')
+                                                {{-- BOTTONE CHE RICHIAMA UN 'POPUP' PER CONFERMARE DI VOLER ELIMINARE L'APPARTAMENTO  --}}
+                                                <button class="btn btn-danger" onclick="return confirm('Sei sicura/o di voler eliminare questo appartamento?')" title="Elimina">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                <div>
                                 @if ($apartment->sponsors()->exists('sponsor_id'))
                                     @foreach ($apartment->sponsors as $sponsor)
                                         <span class="d-none">{{$myID = $sponsor->pivot->sponsor_id}} </span>
                                         @switch($myID)
                                             @case(1)
-                                                <img src="https://banner2.cleanpng.com/20180701/czu/kisspng-sponsor-bronze-business-organization-silver-5b39330112f541.7032012215304752650777.jpg" class="img-fluid" alt=""  style="width: 200px; height: 200px;">
+                                            <img src="{{ asset('storage/public/sponsor/basic.png') }}" alt="basic sponsor"> 
                                                 @break
                                             @case(2)
-                                                <img src="https://img.favpng.com/1/7/8/medal-gold-sponsor-logo-silver-png-favpng-MV15xaYwJQ3wQrK6fWu5FPHFF.jpg" class="img-fluid" alt="" style="width: 200px; height: 200px;">  
+                                                <img src="{{ asset('storage/public/sponsor/premium.png') }}" alt="premium sponsor">  
                                                 @break
                                             @case(3)
-                                                <img src="https://agredfoundation.com/assets/uploads/2020/10/Gold-Sponsor.png"  class="img-fluid" alt="" style="width: 200px; height: 200px;">
+                                            <img src="{{ asset('storage/public/sponsor/business.png') }}" alt="business sponsor"> 
                                                 @break
                                             @default
                                         @endswitch         
                                     @endforeach
                                 @else
-                                    <a class="btn" href=" {{ route('user.sponsors.show', $apartment) }} ">Sponsorizza</a>            
-                                @endif                                              
+                                    <a class="btn btn-primary" id="myBtn" href=" {{ route('user.sponsors.show', $apartment) }} ">Sponsorizza</a>            
+                                @endif
+                                    </div>
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -67,8 +93,4 @@
             </div>
         </div>
     </div>
-@endsection
-
-@section('page-title')
-    Apartments
 @endsection
