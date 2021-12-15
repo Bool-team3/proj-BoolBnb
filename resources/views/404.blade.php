3ad829f9b6a346419adc0bd9b6f1c42c9ff97cdb
daviddpi <piscopodavid97@gmail.com>
2021-12-15 15:15:50 +0100

    add 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,1 +1,17 @@
-<h1>404</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Page not found</title>
+</head>
+<body>
+    <div id="page-not-found">
+        <h3>Page not found</h3>
+        <img src="https://www.ecommerce-nation.com/wp-content/uploads/2018/10/404-error.jpg" alt="404">
+        <a class="btn btn-dark" href="{{route("home")}}">Torna alla home</a>
+    </div>
+</body>
+</html>
--- resources/views/user/apartments/show.blade.php
+++ resources/views/user/apartments/show.blade.php
@@ -2,43 +2,59 @@
 @section('title', "Appartamenti")
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="card-deck">
-                <div class="card">
-                    <img class="card-img-top img-fluid" style="width:300px" src="{{ $apartment->img_url ? $apartment->getImagePrefix() . $apartment->img_url : 'https://www.pianetacasafacile.it/uploads/cache/profile_mid/uploads/property_images/2018/01/property_no_photo.png' }}" alt="Appartamento">
+    <div class="container" id="apartment-show">
+        <a href="{{route('user.apartments.index')}}" class="back-to-home" title="Torna indietro">
+            <i class="fas fa-long-arrow-alt-left fa-4x"></i>
+        </a>
+        <div class="row p-5">
+            <div class="card mt-3">
+                <div class="row g-0">
+                <div class="col-12 col-lg-8">
+                    <img src="{{ $apartment->img_url ? $apartment->getImagePrefix() . $apartment->img_url : 'https://www.pianetacasafacile.it/uploads/cache/profile_mid/uploads/property_images/2018/01/property_no_photo.png' }}" alt="Appartamento" class="img-fluid rounded-start" alt="Foto Appartamento">
+                    <div class="d-flex justify-content-around p-3">
+                        <p class="card-text mb-2">
+                            <div class="d-flex align-items-center">
+                                Visibilità:
+                                @if ($apartment->visible)
+                                    <i class="fas fa-eye fa-2x mx-2" title="Visibile"></i>
+                                @else
+                                    <i class="fas fa-eye-slash fa-2x mx-2" title="Non visibile"></i>
+                                @endif
+                            </div>
+                        </p>
+                        <p class="card-text">
+                            @if ($apartment->sponsors()->exists('sponsor_id'))
+                                <span class="badge badge-success p-1">Sponsorizzato</span>
+                            @else
+                                <span class="badge badge-danger p-1">Non Sponsorizzato</span>
+                            @endif
+
+                            @if ($apartment->sponsors()->exists('expiration_date'))
+                                @foreach ($apartment->sponsors as $sponsor)
+                                    <span class="text-muted"><strong>Scadenza sponsorizzazione: {{$sponsor->pivot->expiration_date}}</strong><span>
+                                @endforeach
+                            @endif        
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-4">
                     <div class="card-body">
-                        <h5 class="card-title">{{$apartment->title}}</h5>
-                        <h6 class="card-title">Stanze: {{$apartment->room}}</h6>
-                        <h6 class="card-title">Stanze da letto: {{$apartment->bedroom}}</h6>
-                        <h6 class="card-title">Posti letto: {{$apartment->bed}}</h6>
-                        <h6 class="card-title">Bagni: {{$apartment->bathroom}}</h6>
-                        <h6 class="card-title">Metri quadrati: {{$apartment->mq}}</h6>
+                        <h5 class="card-title"><strong>{{$apartment->title}}</strong></h5>
+                        <legend>Info sull'indirizzo</legend>
+                        <p class="card-text">Provincia: {{$apartment->province}}</p>
                         <p class="card-text">Città: {{$apartment->city}}</p>
-                        <legend>Servizi:</legend>
-                        <ul>
-                            @forelse ($apartment->services as $service)
-                                <li>{{$service->name}}</li>
-                            @empty
-                                nessun servizio aggiuntivo
-                            @endforelse
-                        </ul>
-                        <p class="card-text"><small class="text-muted">Last updated {{$apartment->created_at}} ago</small></p>
-                        <a href="{{route('user.apartments.edit', $apartment)}}">Modifica Informazioni</a>
-                        <form method="POST" action="{{route('user.apartments.destroy', $apartment)}}" class="delete-form" data-apartment-id="{{$apartment->id}}" data-apartment-title="{{$apartment->title}}">
-                            @csrf
-                            @method('DELETE')
-                            {{-- BOTTONE CHE RICHIAMA UN 'POPUP' PER CONFERMARE DI VOLER ELIMINARE L'APPARTAMENTO  --}}
-                            <input type="submit" value="Delete" onclick="return confirm('Sei sicura/o di voler eliminare questo appartamento?');">
-                        </form>
+                        <p class="card-text">Indirizzo: {{$apartment->street_name}}</p>
+                        <p class="card-text">Numero civico: {{$apartment->street_number}}</p>
+                        <p class="card-text">Codice postale: {{$apartment->postal_code}}</p>
+                        <legend>Info sull'appartamento</legend>
+                        <p class="card-text">Numero di stanze: {{$apartment->room}}</p>
+                        <p class="card-text">Numero di bagni: {{$apartment->bathroom}}</p>
+                        <p class="card-text">Stanze da letto: {{$apartment->bedroom}}</p>
+                        <p class="card-text">Numeri di letto: {{$apartment->bed}}</p>
+                        <p class="card-text">Metri quadrati: {{$apartment->mq}}</p>
                     </div>
                 </div>
             </div>
-            <a href="{{route("user.apartments.index")}}">Torna indietro</a>
         </div>
     </div>
-@endsection
-
-@section('page-title')
-    Show
 @endsection
