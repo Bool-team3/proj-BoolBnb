df59f5d3ed80d0604c1860ffc5ad45e6ad49a4da
daviddpi <piscopodavid97@gmail.com>
2021-12-15 18:49:50 +0100

    add email + filter email + email css

--- resources/views/user/emails/index.blade.php
+++ resources/views/user/emails/index.blade.php
@@ -14,25 +14,42 @@
     <div class="row">
         <div class="col-12">
             <h2>I tuoi messaggi</h2>
-            {{-- <a href="{{route('user.apartments.create')}}">Carica un nuovo appartamento</a> --}}
+            <h4>Hai {{$allEmail}} messaggi in totale</h4>
+
+            {{-- filtro per email --}}
+            <form action="{{ route('user.emails.index')}}" method="GET" enctype="multipart/form-data">
+            @csrf
+                <legend>Filtra messaggi per appartamenti</legend>
+                <label for="apartments">Appartamento</label>
+
+                <select name="apartments[]" id="apartments">
+                    <option value="{{null}}">--</option>
+                    @foreach ($apartments as $apartment)
+                        <option value="{{$apartment->id}}">{{$apartment->title}}</option>
+                    @endforeach
+                </select>
+                <button class="btn btn-outline-primary" type="submit">Invia</button>
+            </form>
             @forelse ($emails as $email)
-                <div class="card-deck">
+                <div class="card-deck email">
                     <div class="card">
                         <div class="d-flex">
-
                             <div class="card-body">
-                                <h5 class="card-title"><a href="{{route("user.emails.show", $email->id)}}">{{$email->subject}}</a></h5>
+                                <h4 class="card-title">Appartamento: {{$allApp[($email->apartment_id) - 2]->title}}</h4>
+                                <h5 class="card-title"><a class="stretched-link" href="{{route("user.emails.show", $email->id)}}">Oggetto: {{$email->subject}}</a></h5>
 
                                 <h6 class="card-title">Nome: {{$email->name}}</h6>
                                 <h6 class="card-title">Indirizzo email: {{$email->email_address}}</h6>
-                                <p class="card-text"><small class="text-muted">Ricevuta il:{{$email->created_at}} ago</small></p>
+                                <p class="card-text"><small class="text-muted">Ricevuta il:{{$email->created_at}}</small></p>
 
                                 {{-- FORM PER ELIMINARE UN APPARTAMENTO  --}}
                                 <form method="POST" action="{{route('user.emails.destroy', $email)}}" class="delete-form" data-email-id="{{$email->id}}" data-email-subject="{{$email->subject}}">
                                 @csrf
                                 @method('DELETE') 
                                     {{-- BOTTONE CHE RICHIAMA UN 'POPUP' PER CONFERMARE DI VOLER ELIMINARE L'APPARTAMENTO  --}}
-                                    <input type="submit" value="Delete" onclick="return confirm('Sei sicura/o di voler eliminare questa email?');">
+                                    <button class="btn btn-danger" onclick="return confirm('Sei sicura/o di voler eliminare questo appartamento?')" title="Elimina">
+                                        <i class="fas fa-trash"></i>
+                                    </button>
                                 </form>
                             </div>
                         </div>
@@ -42,6 +59,7 @@
                 <h4>Non hai messaggi</h4>
             @endforelse
         </div>
+        {{ $emails->links() }}
     </div>
 </div>
 
