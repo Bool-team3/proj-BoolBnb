4547de0134f26bb9f68bb675ba3b9d4c5fbd3321
daviddpi <piscopodavid97@gmail.com>
2021-12-16 17:43:34 +0100

    fix error send message

--- resources/views/singleApartment.blade.php
+++ resources/views/singleApartment.blade.php
@@ -1,48 +1,43 @@
 @extends('layouts.app')
 
-@section('content')
-    <div id="prova">
-        
-    </div>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h4>Invia un messaggio</h4>
-                <form action={{'emails.store'}} method="POST">
-                    @csrf
-                    @guest
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Nome</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Inserisci il tuo nome">
-                        </div>
-                        <div class="form-group">
-                        <label for="exampleFormControlInput1">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="nome@esempio.com">
-                        </div>
-                    @else
-                        <div class="form-group">
-                            <label for="exampleFormControlInput1">Nome</label>
-                            <input type="text" class="form-control" id="exampleFormControlInput1" value="{{Auth::user()->name}}" readonly>
-                        </div>
-                        <div class="form-group">
-                        <label for="exampleFormControlInput1">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" value="{{Auth::user()->email}}" readonly>
-                        </div>
-                    @endguest
-
+@section('content')   
+    <div id="prova"></div>
+    
+    <section id="send-email">
+        <div class="container">
+            <form action="{{ route('email.store') }}" method="POST">
+                @csrf
+                <input type="hidden" name="apartment_id" value="{{$apartment->id}}"> 
+                @guest
                     <div class="form-group">
-                        <label for="exampleFormControlInput1">Oggetto della email</label>
-                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Inserisci l'oggetto della email">
+                        <label for="name">Nome</label>
+                        <input type="text" class="form-control" name="name" id="name" placeholder="Inserisci il tuo nome">
                     </div>
                     <div class="form-group">
-                      <label for="exampleFormControlTextarea1">Example textarea</label>
-                      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <label for="email-address">Email address</label>
+                        <input type="email" class="form-control" name="email_address" id="email-address" placeholder="nome@esempio.com">
                     </div>
-                    <button class="btn btn-primary" type="submit">Invia</button>
-                  </form>
-            </div>
+                @else
+                    <div class="form-group">
+                        <label for="name">Nome</label>
+                        <input type="text" class="form-control" name="name" id="name" value="{{Auth::user()->name}}" readonly>
+                    </div>
+                    <div class="form-group">
+                        <label for="email-address">Email address</label>
+                        <input type="email" class="form-control" name="email_address" id="email-address" value="{{Auth::user()->email}}" readonly>
+                    </div>
+                @endguest
+                <div class="form-group">
+                    <label for="subject">Oggetto della email</label>
+                    <input type="text" class="form-control" name="subject" id="subject" placeholder="Inserisci l'oggetto della email">
+                </div>
+                <div class="form-group">
+                    <label for="message">Messaggio</label>
+                    <textarea class="form-control" name="message" id="message" rows="3"></textarea>
+                </div>
+                <button class="btn btn-primary" type="submit">Invia</button>
+            </form>
         </div>
-    </div>
+    </section>
     <script src="{{asset('js/back.js')}}"></script>
 @endsection
