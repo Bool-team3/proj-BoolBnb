1c2a14e27ef0c00e6d4e3801b3db06b26727c112
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-12-17 11:48:49 +0100

    piccole modifiche aggiunte

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -2,27 +2,28 @@
 @section('title', "Dashboard")
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    {{-- <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div> --}}
                 </div>
             </div>
         </div>
+        <a href="{{route('user.apartments.index')}}">I tuoi appartamenti</a>
+        <a href="{{route('user.emails.index')}}">I tuoi messaggi</a>
     </div>
-    <a href="{{route('user.apartments.index')}}">I tuoi appartamenti</a>
-    <a href="{{route('user.emails.index')}}">I tuoi messaggi</a>
-</div>
 
 @endsection
 
