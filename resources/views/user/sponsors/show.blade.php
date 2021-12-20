3528e9ec9747bd193d8cdefda090cd6946d43ae6
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-12-20 15:52:20 +0100

    sposnsor form completed

--- resources/views/user/sponsors/show.blade.php
+++ resources/views/user/sponsors/show.blade.php
@@ -11,7 +11,7 @@
                 <div class="d-flex justify-content-between">
                     @forelse ($sponsors as $sponsor)
                     
-                        <form action="{{ route('user.sponsors.store')}}"  method="POST">
+                        <form action="{{ route('user.sponsors.store')}}"  method="POST" class="confirmSponsor">
                             @csrf
 
                             <div class="card" style="width: 18rem;">
@@ -21,23 +21,53 @@
                                     <input class="d-none" type="text" name="sponsor_id" value="{{ $sponsor->id }}" readonly id="sponsor_id">
                                     <input class="d-none" type="text" name="apartment_id" value="{{ $apartment }}" readonly >
                                     <h6 class="card-title"> Durata: {{ $sponsor->time }} giorni </h6>
+                                    {{-- <a href="" class="stretched-link"></a> --}}
                                 </div>
                             </div>       
-                            {{-- <button type="" class="btn btn-success">Compra</button> --}}
+                            <button type="" class="btn btn-success">Compra</button>
 
                         </form>
                     @empty
                         <h6>Non ci sono sponsor disponibili.</h6>
                     @endforelse
                 </div>
-                <div id="dropin-container" class="col-12 p-0"></div>  
-                <button type="submit" class="btn btn-primary btn-compra" id="gg">Compra</button>
+                <div id="dropin-container" class="col-12 p-0 d-none"></div>  
+                <button type="submit" class="btn btn-primary btn-compra d-none" id="gg">Compra</button>
 
             </div>
         </div>
     </div>
     <script type="text/javascript">
-    //    var button = document.querySelector('#submit-button');
+        // var card = document.getElementsByClass('card'):
+
+        // card.addEventListener('click' =>{
+
+        // });
+        const confirmSponsor = document.querySelectorAll('.confirmSponsor');
+            confirmSponsor.forEach(element => {
+
+                element.addEventListener("submit", function(event){
+                    //stoppa l'evento
+                    event.preventDefault();
+
+                    //apre il box di conferma
+                    let confirm = document.querySelector('#dropin-container');
+                    confirm.classList.remove("d-none");
+
+                    //prende il valore dei bottoni del box di conferma
+                    const buttonPurchase = document.querySelector('#gg');
+                    buttonPurchase.classList.remove("d-none");
+                
+
+                    buttonPurchase.addEventListener("click", function(){
+                        element.submit();
+                        confirm.classList.add("d-none");
+                    })
+
+                    
+                });
+
+            });
        var button = document.getElementById('gg');
 
         braintree.dropin.create({
